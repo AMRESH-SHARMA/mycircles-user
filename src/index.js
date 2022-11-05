460c5465521617c34fc850863297114364f74736
SaiAuditya <31877422+SaiAuditya@users.noreply.github.com>
2022-11-05 23:07:28 +0530

    ok
    uri updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 import "./index.css";
 
 const setupAxios = () => {
-  axios.defaults.baseURL = 'http://localhost:8080/api/spaces/v1/';
+  axios.defaults.baseURL = 'http://206.189.133.189/api/spaces/v1/';
   // axios.defaults.auth= {
   //   username: 'ramki',
   //   password: 'Infy@1234'
