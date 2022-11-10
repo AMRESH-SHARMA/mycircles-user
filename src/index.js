593c32b1d0445c3fed5156dde738747ff2dc8cc9
AMRESH <amreshaiio@gmail.com>
2022-11-10 12:32:53 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
-import '@coreui/coreui/dist/css/coreui.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,8 +8,8 @@ import axios from 'axios'
 import "./index.css";
 
 const setupAxios = () => {
-  // axios.defaults.baseURL = 'http://206.189.133.189/api/spaces/v1';
-  axios.defaults.baseURL = 'https://circlenowdev.xyz/api/v1';
+  axios.defaults.baseURL = 'http://206.189.133.189/api/spaces/v1';
+  // axios.defaults.baseURL = 'https://circlenowdev.xyz/api/v1';
   axios.defaults.headers = {
     'Cache-Control': 'no-cache,no-store',
     'Pragma': 'no-cache',
